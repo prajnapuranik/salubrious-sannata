0fe11c304d619e98ddac795305a39ddbea2adccb
PrajnaGP <prajnagp@gmail.com>
2020-11-18 18:55:53 +0530

    Adding get and post request to the application

---
+++ ss.version1.demo/src/main/java/ss/version1/main/Activity/Activity.java
@@ -0,0 +1,30 @@
+package ss.version1.main.Activity;
+
+public class Activity {
+
+	int id;
+	String name;
+	
+	public Activity() {}
+	
+	public Activity(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
---
+++ ss.version1.demo/src/main/java/ss/version1/main/Activity/ActivityService.java
@@ -0,0 +1,38 @@
+package ss.version1.main.Activity;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+/** In Spring, business services are typically singletons
+ * 		
+ * 		When application starts, Spring creates an instance of this service and keeps it in memory
+ * 				-> other classes can depend on this instance to access the service
+ * 						-> Spring then injects this dependency to those classes
+ * */
+@Service
+public class ActivityService {
+	
+	//Arrays.asList would make the list immutable since array can't be added to once initialized -> wont work with post
+		//so create an arraylist and add the array into it as a list for easy access
+	List<Activity> activityList = new ArrayList<Activity>(Arrays.asList(new Activity(1,"Walking"),
+			new Activity(2,"Swimming"),
+			new Activity(3,"Yoga")));
+	
+	List<Activity> returnAllActivities()
+	{
+		return activityList;
+	}
+	
+	Activity getActivityById(int id)
+	{
+		return activityList.stream().filter(a -> a.getId() == id).findFirst().get();
+	}
+	
+	void addActivity(Activity acti)
+	{
+		activityList.add(acti);
+	}
+}
